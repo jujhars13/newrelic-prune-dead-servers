f580d23bcffe3ab8b66c3828f77f27b048c7c2d3
Jujhar Singh <jujhar@jujhar.com>
2014-11-12 10:46:21 +0000

    fully working

--- prune-dead-servers.go
+++ prune-dead-servers.go
@@ -1,21 +1,23 @@
 //Gets all the dead servers in your newrelic account and then removes them
 //Great for ephemeral spot instance type EC2 architectures which can clog up
-//Newrelic with a load of dead servers
+//Newrelic with a load of dead servers, removing them via the web interface is painful
+//TODO run deleteions as parralel jobs using channels to sync
 package main
 
 import (
 	"encoding/json"
-	"flag" //cli parsing
-	//"fmt"
+	"flag"      //cli parsing
 	"io/ioutil" //some convenience iostream methods
 	"log"
 	"net/http"
+	"strconv"
 )
 
-const API_ENDPOINT = "https://api.newrelic.com/v2/servers.json"
+const API_ENDPOINT = "https://api.newrelic.com/v2/servers"
 
 var apiKey *string = flag.String("api-key", "", "Your newrelic api key")
 
+//our minimum data that we'll marshall out of the NR json
 type nrJson struct {
 	Servers []struct {
 		Id        int    `json:"id"`
@@ -31,10 +33,10 @@ func main() {
 	if *apiKey == "" {
 		log.Fatal("You must supply your NewRelic api key use --api-key")
 	}
-
+	log.Println("Getting server list from NR")
 	//create a http client for custom headers, rather than use the convenience GET method
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", API_ENDPOINT, nil)
+	req, err := http.NewRequest("GET", API_ENDPOINT+".json", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,7 +48,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//read body
+	//read body stream
 	body, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
@@ -57,41 +59,50 @@ func main() {
 		log.Fatal("Didn't get a 200 statuscode ", res.StatusCode)
 	}
 
-	log.Print("raw_json", string(body))
-
+	//marshal the json output into our struct
 	unmarshaledJson := nrJson{}
 	err = json.Unmarshal(body, &unmarshaledJson)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//foreach over servers and remove dud ones
+	var serverCount int = 0
+	//foreach over servers and remove the duds
 	servers := unmarshaledJson.Servers
 	for _, s := range servers {
 		if s.Reporting == false {
-			RemoveServer(s.Id)
+			if RemoveServer(s.Id) == true {
+				serverCount++
+			} else {
+				log.Panicln("Couldn't remove server ", s.Id)
+			}
 		}
 	}
-	//if server["reporting"] == false {
-	//		s := server.(map[string]interface{})
-	//		if s["reporting"] == false {
-	//log.Printf("Removing dead server ", s)
-	//			log.Printf("Removing dead server ", s["name"])
-	//			fmt.Print(s["id"])
-	//			server_id, _ := js.Int(s["id"])
-	//			RemoveServer(server_id)
-	//		}
-	//		if server["id"] {
-	//			log.Printf("index", server.id)
-	//		}
-	//	}
-	//}
-	//log.Printf("response", nrResponse.servers[0].id)
-
-	//2. foreach over results and delete dud servers
+	log.Println("Removed", serverCount, "dead servers from your NR account")
 }
 
+//Remove a dead server by id
 func RemoveServer(serverId int) bool {
-	log.Printf("server id ", serverId)
+	log.Println("Removing dead server id", serverId)
+	//create a manual http object to pass in the API key
+	client := &http.Client{}
+	var url string = API_ENDPOINT + "/" + strconv.Itoa(serverId) + ".json"
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("X-Api-Key", *apiKey)
+
+	// DELETE server data from the api
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.StatusCode != 200 {
+		log.Panic("Didn't get a 200 statuscode ", res.StatusCode)
+		return false
+	}
+
 	return true
 }
